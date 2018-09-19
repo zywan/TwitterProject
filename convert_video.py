a4d65f097fa60e2241e7dd4d7e00ef6bc759b19e
wanzy <43022123+zywan@users.noreply.github.com>
2018-09-18 22:37:32 -0400

    Update convert_video.py

--- convert_video.py
+++ convert_video.py
@@ -1,4 +1,5 @@
 
 import os
+# change to your personal path where you save the images
 os.chdir('/Users/wang/Desktop/twitterProject/images/')
 os.popen('ffmpeg -loop 1 -i %d.jpg -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -r 0.5 -t 60 vedio_name.mp4','r',1)
