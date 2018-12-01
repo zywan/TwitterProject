fcd1996b9e0a12f30312f2dc8cd70981e8689f90
wanzy <wanzy@bu.edu>
2018-12-01 00:22:28 -0500

    modify test mysql

--- test_mysql.py
+++ test_mysql.py
@@ -1,16 +1,44 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		passwd="12345678",
-		database='twitter_mysql'
-	)
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM user")
-myresult = mycursor.fetchall()
-for x in myresult:
-	print(x)
-mycursor.execute("SELECT * FROM activity")
-myresult = mycursor.fetchall()
-for x in myresult:
-	print(x)
+def mysql_statistic(search_key):
+	mydb = mysql.connector.connect(
+			host="localhost",
+			user="root",
+			passwd="12345678",
+			database='twitter_mysql'
+		)
+	mycursor = mydb.cursor()
+	search_time = 0
+	image_num = 0
+	key_list= {}
+	sessions = {}
+	mycursor.execute("SELECT * FROM activity")
+	activity_list = mycursor.fetchall()
+	for activity in activity_list:
+		search_time +=1
+		image_num +=int(activity[4])
+		if activity[1] not in key_list:
+			key_list[activity[1]] = 1
+		else:
+			key_list[activity[1]] +=1
+
+		if activity[1] == search_key:
+			if activity[0] not in sessions:
+				sessions[activity[0]] = 1
+			else:
+				sessions[activity[0]] += 1
+
+	image_per_feed = float(image_num/search_time)
+	maximum_search = 0
+	print('Overall: ', key_list)
+	print('Number of images per feed: ', image_per_feed)
+	print('which sessions search',search_key ,": ",sessions)
+	for key in key_list:
+		maximum_search = max(key_list[key],maximum_search)
+	m_pop_twt = []
+	for key in key_list:
+		if (key_list[key]==maximum_search):
+			m_pop_twt.append(key)
+	print('Most popular: ', m_pop_twt,'searched for: ', maximum_search, 'times')
+
+mysql_statistic('NBAonTNT')
+
